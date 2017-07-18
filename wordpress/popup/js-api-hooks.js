b62b5343a757a2a6c4afa0cecf24965c7d6714ec
latimglobal <latimglobal@gmail.com>
2017-07-18 23:38:26 +0600

    Update js-api-hooks.js

--- wordpress/popup/js-api-hooks.js
+++ wordpress/popup/js-api-hooks.js
@@ -1,1 +1,28 @@
+//Case study mjay's
+
+$('#pum-318')
+        .on('pumBeforeOpen', function () {
+            var $popup = $(this);     $('body').prop('style','overflow:hidden;height:100%;');
+            });
+$('#pum-318')
+        .on('pumAfterClose', function () {
+            var $popup = $(this);     $('body').prop('style','overflow:auto;height:auto;');
+            });
+$('#pum-320')
+        .on('pumBeforeOpen', function () {
+            var $popup = $(this);     $('body').prop('style','overflow:hidden;height:100%;');
+            }); 
+            
+
+$('#pum-320')
+        .on('pumBeforeClose', function () {
+            var $popup = $(this);     
+            });
+$('#pum-320')
+        .on('pumAfterClose', function () {
+        $(window).unbind('scroll');
+        $('body').prop('style','overflow:auto;height:auto;');	
+            window.scrollTo(0,$('#mjaysSpecialRow').offset().top);
+
+            }); 
 
