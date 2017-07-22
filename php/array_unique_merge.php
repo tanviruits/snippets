5837920d5591bb01033b360a5cf0614ecbfe790c
latimglobal <latimglobal@gmail.com>
2017-07-22 20:01:45 +0600

    Create array_unique_merge.php

---
+++ php/array_unique_merge.php
@@ -0,0 +1,9 @@
+<?php
+$input = array("green", "blue", "red", "red");
+$result = array_unique($input);
+print_r($result);
+$array1 = array(1,2,3);
+$array2 = array(3,4,5);
+$array_merged = array_merge($array1, $array2);
+print_r($array_merged);
+?>
