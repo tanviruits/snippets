27332e52436867c84bb8ef91646eb470208c5d07
latimglobal <latimglobal@gmail.com>
2018-05-19 15:04:40 +0600

    Update enque.php

--- wordpress/child_themes/enque.php
+++ wordpress/child_themes/enque.php
@@ -4,7 +4,7 @@
  */
 function wpdocs_theme_name_scripts() {
     wp_enqueue_style( 'style-name', get_stylesheet_uri() );
-    wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+    wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array('jquery'), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
 ?>
